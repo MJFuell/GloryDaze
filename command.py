6daf34f3c983f28b76dd0a0252b9636b4bb76a85
Michael Fuelling <fuellinm@oregonstate.edu>
2019-03-02 22:40:36 -0600

    Update command.py
    Added outline for primary verbs
    Added 'go' function which lets you move by roomname or direction

--- command.py
+++ command.py
@@ -1,310 +1,170 @@
-'''
-game.py
-The main game file for GloryDaze Text Adventure.
-started during engine development, debug and test.
-by: Michael Fuelling
 
-CS467
-Winter 2019
-Team Keld :: Michael Fuelling, Richard Ratliff, Jordan Riojas
-'''
 
-import time
-import sys
-
-import command
+import util
+import data_format
 from parser_files import parser
 from parser_files import lexicon
-import data_format as DF
-import util
+
 
 directions = ["north", "south", "east", "west", "northeast", "southeast", "northwest", "southwest"]
 
-class GameState:
-	#Player
-	player = 0
+err = "Can't do that"
 
-	#Map
-	room_list = None
-	exit_list = None
-	item_list = None
-	current_room = None
-	current_exit = None
+def verb_save(GS, obj):
+	pass
+	#Save all game info to save files/directory
+	#let the user know the game has been saved
 
-	#conditions
-	win = 0
-	lose = 0
-	turnCount = 0
+def verb_load(GS, obj):
+	pass
+	#Ask the user if they are sure they want to load as it will overwrite current progress
+	#load data into new gamestate
+	#call gameLoop() with loaded gamestate
 
-	#Initializer
-	def __init__(self):
-		pass
+def verb_help(GS, obj):
+	pass
+	#print help info to the user
+
+def verb_inventory(GS, obj):
+	pass
+	#display current inventory
 
 
-def GameLoop(GS):
-	"""
-	"Gaming Loop" which loops for user input and attempts to execute it.  
-	"""
-	start = time.time()
-	uInput = 0
-	while uInput != 'q':
-		uInput = input('("q" to quit, "view" for adjacent rooms) >')
 
-		s = parser.parse_sentence(lexicon.scan(uInput.lower()))
-		print(s.subject)
-		print(s.verb)
-		print(s.object + '\n')
 
-		command.command(GS, s)
-		
-				
-		if uInput == 'view':
-			print('')
-			#print(GS.current_room.get_exits().values())
-			for val in set(GS.current_room.get_exits().values()):
-				print(val)
 
-	
-            
-		elapsed = int(time.time() - start)
-		# print('elapsed time is {:02d}:{:02d}:{:02d}'.format(elapsed // 3600, (elapsed % 3600 // 60), elapsed % 60))
-		
-		# -------------------- RELEVANT TO GAME.  UNCOMMENT AFTER DEBUG -----------------------------------------
-		#print('\nelapsed time is {:02d}:{:02d}'.format((elapsed % 3600 // 60), elapsed % 60))
-		# -------------------------------------------------------------------------------------------------------		
+def verb_go(GS, obj):
 
-		GS.elapsed = elapsed
-		if (GS.elapsed > 1200):
-			GS.lose = 1
+	move = False
 
-		# GS.win = 1 # test win condition,  TODO to set it up
+	#Room name or alt room name
+	#Make sure that room is adjacent
+	for x in GS.room_list:
+		#print('x.name = ' + x.name)
+		#print('x.altnames = ' + ", ".join(str(e) for e in x.altnames))
+		if obj == x.name.lower() or obj in x.altnames:
+			#print('x is ' + x.name)
+			if x.name in GS.current_room.exits.values():
+				GS.current_room = x
+				move = True
 
-		#End of turn maintenance
-		if GS.win == 1:
-			util.print_you_won()
-			print('')
-			print('Your time was', round(GS.elapsed/60, 1), 'minutes.')
-			print('')
-			quit()
-		elif GS.lose == 1:
-			util.print_sorry_you_lost()
-			print('')
-			print('Your elapsed time was more than 20 minutes.')
-			print('')
-			quit()
-		GS.turnCount += GS.turnCount
-        
+			
+	#Directions
+	#Make sure you can travel that direction from current room
+	if obj in GS.current_room.exits:
+		roomName = GS.current_room.exits.get(obj)
+		for x in GS.room_list:
+			if x.name == roomName:
+				GS.current_room = x
+				move = True
+
+	#If we successfully moved rooms
+	if move == True:
+		print('-' * 70, '\n\n\n')
+		exits = GS.current_room.get_exits()
+		#print('Moved to ' + GS.current_room.get_name())
+		util.print_ascii_art('./data/artwk/' + GS.current_room.get_name())
 		print('')
-		print('End of turn.')
-		print('')
+		if GS.current_room.visited:
+			util.scroll3(0.01, 60, GS.current_room.get_short())
+			print('')
+			for item in GS.current_room.get_items():
+				for x in GS.item_list:
+					if x.name == item or item in x.altnames:
+						util.scroll3(0.01, 60, "{}".format(x.get_avail()))
+						print('')
+			for exits_dir, exits_room in exits.items():
+				for x in GS.exit_list:
+					if x.name == exits_room and exits_dir in directions:
+						util.scroll3(0.01, 60, "{} {}".format(x.get_short(),exits_dir))
+						print('')
+		else:
+			util.scroll3(0.01, 60, GS.current_room.get_long())
+			print('')
+			for item in GS.current_room.get_items():
+				for x in GS.item_list:
+					if x.name == item or item in x.altnames:
+						util.scroll3(0.01, 60, "{}".format(x.get_avail()))
+						print('')
+			for exits_dir, exits_room in exits.items():
+				for x in GS.exit_list:
+					if x.name == exits_room and exits_dir in directions:
+						util.scroll3(0.01, 60, "{} {}".format(x.get_long(),exits_dir))
+						print('')
+		GS.current_room.visited = True
 
-
-def RunGame(type):
-    """
-    Setups up the gamestate, rooms, player, objects, and characters depending on if a new game or loaded game.  Once the game is setup, calls GameLoop() with the setup gamestate to loop for user input
-    """
-
-    #NEW GAME
-    if type == 0:           
-
-        #Setup Gamestate
-        gamestate = GameState()
-
-        #Setup player
-        p = DF.Player()
-        gamestate.player = p
-
-        #setup rooms
-        print('\nBuilding rooms from files')
-        room_builder = DF.RoomBuilder()
-        gamestate.room_list = DF.RoomBuilder.load_room_files(room_builder)
-        exit_builder = DF.ExitBuilder()
-        gamestate.exit_list = DF.ExitBuilder.load_exit_files(exit_builder)
-        item_builder = DF.ItemBuilder()
-        gamestate.item_list = DF.ItemBuilder.load_item_files(item_builder)
-
-        #print('\nChecking rooms loaded into gamestate:')
-        #for x in gamestate.room_list:
-            #print(x.name)
-
-        #Start in detention
-        for x in gamestate.room_list:
-            if x.name == 'Detention':
-                gamestate.current_room = x
-                gamestate.current_room.visited = True
-        print('\nCurrent room is: ' + gamestate.current_room.name)
-
+	#If we did not move
+	else:
+		print('Can\'t go that way')
         
-        #Play opener to get player name
-        print('\nNEW GAME OPENER:\n')
-        delay = 1.5
+
+def verb_look(GS, obj):
+	pass
+	#If no object, print long description of room
+	#otherwise print description of item
+
+def verb_take(GS, obj):
+	pass
+	#make sure user has a backpack
+	#verify item is in the room
+	#add item to inventory if possible
+
+
+
+
+def command(GS, s):
+	if s.subject == 'player':
+
+		if s.verb == 'save' or 'savegame':
+			verb_save(GS, s.object)
+
+		if s.verb == 'load' or 'loadgame':
+			verb_load(GS, s.object)
+
+		if s.verb == 'help':
+			verb_help(GS, s.object)
+
+		if s.verb == 'inventory':
+			verb_inventory(GS, s.object)
+
+
+
+		if s.verb == "go":
+			verb_go(GS, s.object)
         
-        opener1 = '"Hey."\n'
-        opener2 = '"Hey..."\n'
-        opener3 = '"HEY!!!!"\n'
+		if s.verb == 'look':
+			verb_go(GS, s.object)
 
-        for letter in opener1:
-            sys.stdout.write(letter)
-            sys.stdout.flush()
-            time.sleep(0.05)
-        #print()
-        time.sleep(delay)
-            
-        for letter in opener2:
-            sys.stdout.write(letter)
-            sys.stdout.flush()
-            time.sleep(0.05)
-        #print()
-        time.sleep(delay)
-            
-        for letter in opener3:
-            sys.stdout.write(letter)
-            sys.stdout.flush()
-            time.sleep(0.05)
-        #print()
-        time.sleep(delay)
+		if s.verb == 'take':
+			verb_take(GS, s.object)
 
-        print('Hmm.... Uh, what? Where am I?')
-        time.sleep(delay + 1)
-        
-        print('"Where do you think?  You\'re in detention kid.  What\'s your name anyway?"')
-        time.sleep(delay + 1)
-        
-        print('My... My name?  Its....')
-        name = input('>')
-        gamestate.player.set_name(name)
-        time.sleep(delay)
-        
-        print('"Well, ' + gamestate.player.get_name() + ', it\'s 6:30 PM."')
-        print('"Detention ended a half hour ago.  Time to go home."')
-        time.sleep(delay + .5)
-        
-        print('What? Its 6:30!? My parents are gonna kill me. I better get out of here!\n')
-        time.sleep(delay)
+		
+
+		
+		if s.verb == 'talk':
+			verb_talk(GS, s.object)
+
+		if s.verb == 'drop':
+			verb_drop(GS, s.object)
+
+		if s.verb == 'open':
+			verb_open(GS, s.object)
+
+	else:
+		print('I Don\'t understand')
+
+'''
+backpack - pack bag bookbag
+book - harry harrypotter potter harrypotterbook
+bottledwater - water bottle coldwater cold water
+calculator - calc calcalator calclator
+camera - cam gopro
+cellphone - cell phone cell phone
+ducttape - tape ducttape ducktape
+officepass - officepass pass
+piccolo - picolo piccollo picollo picalo piccalo
+sdcard - sd card sdcard memorycard memory card
+'''
 
 
-
-    #LOAD GAME
-    if type == 1:           
-        #Load from save file
-        pass
-
-    
-    #Print room description so user knows where they are and start looping for input
-    print('You are in ' + gamestate.current_room.get_name())
-    print('')
-    util.scroll3(0.01, 60, gamestate.current_room.get_long())
-    print('')
-    #print(gamestate.current_room.get_items())
-    #print('')
-    for item in gamestate.current_room.get_items():
-        for x in gamestate.item_list:
-            if x.name == item:
-                # print("{}".format(x.get_long()))
-                util.scroll3(0.01, 60, "{}".format(x.get_avail()))
-    print('')
-    #print(gamestate.current_room.get_exits())
-    #print('')
-    exits = gamestate.current_room.get_exits()
-    for exits_dir, exits_room in exits.items():
-        for x in gamestate.exit_list:
-            if x.name == exits_room and exits_dir in directions:
-                util.scroll3(0.01, 60, "{} {}".format(x.get_long(),exits_dir))
-    print('')
-
-    GameLoop(gamestate)  
-
-    
-
-def Title():
-    """
-    First functions called to play the game.
-    Check terminal size and present title screen for the user.
-    Calls MainMenu() and game loops to MainMenu(), not here, for some choices.
-    """
-    util.term_check()
-    util.print_title()
-    MainMenu()
-
-
-def MainMenu():
-    """
-    Calls RunGame() once the user chooses new game or load game.
-    """
-    util.print_start_menu() # uses menu in ./data/artwk/start_menu
-
-    try:
-        selection = int(input('>'))
-    except ValueError:
-        print('Please input a valid integer 1, 2, 3, or 4')
-        MainMenu()
-
-    if selection == 1:
-        print('Starting NEW GAME')
-        RunGame(0)      
-
-    elif selection == 2:
-        #RunGame(1)
-        print('LOAD GAME not yet implemented.\n')
-        MainMenu()
-
-    elif selection == 3:
-        util.scroll3(0.005, 60, 'GloryDaze is a text only adventure.  '+
-        'That means there are no graphics! Everything about the game '+
-        'will be displayed on the screen. Want to do something? Just '+
-        'type it in! (a ">" symbol means the game is waiting for your '+
-        'input). The game allows for as much natural language as possible '+
-        'but if you\'re having trouble getting around, try shorter sentences '+
-        'like "open door" or "push button". (NOT IMPLEMENTED) At any '+
-        'point during the game, type "savegame" to save your progress '+
-        'or "help" for help.  Have fun!\n')
-        print('')
-        MainMenu()
-
-    elif selection == 4:
-        print('Have a nice day!')
-        quit()
-
-    elif selection == 5:
-        #Setup Gamestate
-        gamestate = GameState()
-
-        #Setup player
-        p = DF.Player()
-        gamestate.player = p
-
-        #setup room
-        print('\nBuilding rooms from files')
-        room_builder = DF.RoomBuilder()
-        gamestate.room_list = DF.RoomBuilder.load_room_files(room_builder)
-        exit_builder = DF.ExitBuilder()
-        gamestate.exit_list = DF.ExitBuilder.load_exit_files(exit_builder)
-        item_builder = DF.ItemBuilder()
-        gamestate.item_list = DF.ItemBuilder.load_item_files(item_builder)
-
-        #print('\nChecking rooms loaded into gamestate:')
-        #for x in gamestate.room_list:
-            #print(x.name)
-
-        #Start in detention
-        for x in gamestate.room_list:
-            if x.name == 'Detention':
-                gamestate.current_room = x
-                gamestate.current_room.visited = True
-        print('\nCurrent room is: ' + gamestate.current_room.name)
-        gamestate.player.set_name('TEST')
-        GameLoop(gamestate)
-
-    else:
-        print('Please input a valid integer 1, 2, 3, or 4')
-        MainMenu()
-
-
-# Title, which calls MainMenu, instead of MainMenu here allows
-# terminal size check one time only and printing of GloryDaze
-# one time especially when option 3 help is used in a minimum
-# size terminal - GloryDaze art scrolls part of help off screen
-# MainMenu()
-
-Title()
 
